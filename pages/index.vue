30027031ae0b41d52bd51179f244df9d67795dd5
alibekkenny <topskiali@gmail.com>
2021-07-09 20:18:05 +0600

    Logo commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,16 @@
 <template>
   <div class="bg-gray-100">
-    <div class="px-4 py-3 w-full flex bg-blue-500 text-white">
-      <span>Soon</span>
+    <div class="px-4 py-3 w-full inline-block bg-blue-500 text-white">
+      <img
+        width="60 px"
+        class="rounded-full inline-block"
+        src="https://avatanplus.com/files/resources/original/58cd68232aa0815ae25ec967.png"
+        alt="logotype"
+      />
+      <span class="inline-block">Soon</span>
     </div>
-    <div class="px-10 py-4 flex w-full">
-      <select
-        @change="changed()"
-        class="form-select px-4 py-3 rounded-full bg-white"
-        v-model="key"
-      >
-        <option value="1">По умолчанию</option>
-        <option value="2">По убыванию</option>
-        <option value="3">По возрастанию</option>
-      </select>
-    </div>
-    <div class="w-full px-10">
+
+    <div class="w-full px-10 mt-3">
       <label class="w-full inline-block font-bold">Цена</label>
       <div class="py-2 inline-block">
         <input
@@ -103,6 +99,50 @@
         </div>
       </div>
     </div>
+    <div class="px-10 py-4 flex w-full">
+      <div class="relative text-gray-600">
+        <input
+          type="search"
+          name="search"
+          placeholder="Поиск"
+          class="bg-white h-10 px-5 pr-10 rounded-full"
+          v-model="searchbar"
+        />
+        <button @click="filterQuery()" class="absolute right-0 top-0 mt-3 mr-4">
+          <svg
+            class="h-4 w-4 fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            version="1.1"
+            id="Capa_1"
+            x="0px"
+            y="0px"
+            viewBox="0 0 56.966 56.966"
+            style="enable-background: new 0 0 56.966 56.966"
+            xml:space="preserve"
+            width="512px"
+            height="512px"
+          >
+            <path
+              d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
+            />
+          </svg>
+        </button>
+      </div>
+      <div class="w-full inline-block">
+        <div class="float-right">
+          <select
+            @change="changed()"
+            class="form-select px-4 py-3 rounded-full bg-white"
+            v-model="key"
+          >
+            <option value="1">По умолчанию</option>
+            <option value="2">По убыванию</option>
+            <option value="3">По возрастанию</option>
+          </select>
+        </div>
+      </div>
+    </div>
     <div
       class="
         p-10
@@ -159,6 +199,7 @@ export default {
       input_to: {},
       min: {},
       max: {},
+      searchbar: "",
     };
   },
   mounted() {
@@ -201,16 +242,16 @@ export default {
     async filterQuery() {
       let company;
       if (document.getElementById("apple").checked) {
-        company = "Apple";
+        company = "apple";
       }
       if (document.getElementById("samsung").checked) {
-        company = "Samsung";
+        company = "samsung";
       }
       if (document.getElementById("xiaomi").checked) {
-        company = "Xiaomi";
+        company = "xiaomi";
       }
       if (document.getElementById("huawei").checked) {
-        company = "Huawei";
+        company = "huawei";
       }
       if (document.getElementById("all").checked) {
         company = "";
@@ -219,11 +260,12 @@ export default {
       this.input_to = this.input_to ? Number(this.input_to) : this.max;
       const res = await this.$apollo.query({
         query: gql`
-          query ($from: Int, $to: Int, $company: String) {
+          query ($from: Int, $to: Int, $company: String, $search: String) {
             products(
               filter: {
                 price: { gte: $from, lte: $to }
                 company: { contains: $company }
+                title: { contains: $search }
               }
             ) {
               id
@@ -240,6 +282,7 @@ export default {
           from: this.input_from,
           to: this.input_to,
           company: company,
+          search: this.searchbar,
         },
       });
       this.products = res.data.products;
